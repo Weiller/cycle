aa6458d3a8f11ce836df8f336bc82a7302aed27d
Weiller Schepis <weillerschepis@gmail.com>
2017-11-29 00:31:04 -0200

    Removendo instrução desnecessária

--- cycle-client/src/app/core/navbar/navbar.component.ts
+++ cycle-client/src/app/core/navbar/navbar.component.ts
@@ -9,12 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   constructor(private authService: SegurancaService) { }
-  nomeUsuario: string;
 
   ngOnInit() {
-    if (this.authService.jwtPayload) {
-      this.nomeUsuario = this.authService.jwtPayload.nome;
-    }
   }
 
 }
